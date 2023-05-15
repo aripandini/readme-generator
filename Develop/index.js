15713068ff5b8e114abe118e83b60023365f2b71
aripandini <ariannepandini@hotmail.com>
2023-05-14 22:18:16 -0400

    prompt questions for README added.

--- Develop/index.js
+++ Develop/index.js
@@ -1,11 +1,67 @@
 // TODO: Include packages needed for this application
 
+const generateMarkdown = require("./utils/generateMarkdown");
 const inquirer = require('inquirer');
 const { writeFile } = require('fs').promises;
 
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+        {
+          type: 'input',
+          name: 'title',
+          message: 'What is the title of your project?',
+        },
+        {
+          type: 'input',
+          name: 'description',
+          message: 'Provide a short description of your project.',
+        },
+        {
+          type: 'input',
+          name: 'install',
+          message: 'Which command should be run to install dependencies?',
+        },
+        {
+          type: 'input',
+          name: 'usage',
+          message: 'Provide examples for usage.',
+        },
+        {
+            type: 'checkbox',
+            name: 'license',
+            message: 'Which license will you be using?',
+            choices: [
+                'MIT License',
+                'Apache License 2.0',
+                'GNU General Public License',
+                'Berkeley Software Distribution License',
+                'Boost Software License'
+            ]
+        },
+        {
+          type: 'input',
+          name: 'contribution',
+          message: 'How can others collaborate with this project?',
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'Which command should be run for tests?',
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Provide your Github profile.',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Provide your email.',
+          },
+      ]);
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
