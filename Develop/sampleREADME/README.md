96d4d2a6ab76721e952ffaaf4db04f91cf8ff41a
aripandini <ariannepandini@hotmail.com>
2023-05-18 14:39:38 -0400

    logic for rendering licenses

---
+++ Develop/sampleREADME/README.md
@@ -0,0 +1,44 @@
+# djfaoi
+  
+  ## Description
+
+  difh
+
+  ## Table of Contents 
+
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+
+  ## Installation
+
+  aef
+
+  ## Usage
+
+  ifdj
+
+  
+  ## License
+This project is licensed under Apache License 2.0..
+  .
+
+
+  ## Contributing
+  
+  dwaih
+
+  ## Tests
+
+  osjadf
+
+  ## Questions
+  
+  Please reach out with any questions you might have! 
+
+  - [GitHub Profile](https://github.com/oadsfj)
+  - Email: odsfj
+  
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,18 +1,33 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseBadgeLink(license) {
+  if (license === 'MIT License') {
+    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+  } else if (license === 'Apache License 2.0') {
+    return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+  } else if (license === 'GNU General Public License v3') {
+    return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+  } else if (license === 'Boost Software License') {
+    return '[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)';
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== 'Unlicensed') {
+    return `## License
+This project is licensed under ${license}.`;
+  } else {
+    return "`## License`";
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown({title, description, install, usage, license, contribution, test, github, email}) {
-  return `#${title}
+  return `# ${title}
   
   ## Description
 
@@ -35,9 +50,10 @@ function generateMarkdown({title, description, install, usage, license, contribu
 
   ${usage}
 
-  ## License
+  
+  ${renderLicenseSection(license)}
+  ${renderLicenseBadgeLink(license)}
 
- This project is licensed under ${license}.
 
   ## Contributing
   
